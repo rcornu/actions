1631143e1fc421aa8e21953d72c3d70a7d4f0233
Romain CORNU <romain.cornu@live.com>
2018-12-28 14:41:42 +0100

    clean code and update documentation

--- wait/README.md
+++ wait/README.md
@@ -1,1 +1,27 @@
-# wait - wait until a network connection (TCP or UDP) is open
+# GitHub Actions for Wait
+
+Wait until a network connection (TCP or UDP) is open
+
+## Usage
+
+An example workflow to wait until a network connection is open to `google.com` follows:
+
+```hcl
+action "Wait" {
+  uses = "rcornu/actions/wait@master"
+  args = ""
+  env = {
+    INTERVAL = 5
+    HOST = "google.com"
+    PORT = 80
+    TIMEOUT = 300
+  }
+}
+```
+
+### Environment variables
+
+- `INTERVAL`- **Optional** To specify an interval between tries, defaults to `5` seconds
+- `HOST`- **Optional** To specify connection host, defaults to `google.com`
+- `PORT`- **Optional** To specify connection port, defaults to `80`
+- `TIMEOUT`- **Optional** To specify a timeout, defaults to `300` seconds
--- wait/entrypoint.sh
+++ wait/entrypoint.sh
@@ -2,16 +2,37 @@
 
 set -e
 
-[ -z "$INTERVAL" ]  && { INTERVAL=5; echo "WARNING: INTERVAL is undefined, default is $INTERVAL seconds"; }
-[ -z "$HOST" ]      && { HOST="google.com"; echo "WARNING: HOST is undefined, default is $HOST"; }
-[ -z "$PORT" ]      && { PORT=80; echo "WARNING: PORT is undefined, default is $PORT"; }
-[ -z "$TIMEOUT" ]   && { TIMEOUT=300; echo "WARNING: TIMEOUT is undefined, default is $TIMEOUT seconds"; }
+log () {
+    echo "$1: $2"
+    if [ "$1" == 'ERROR' ]; then
+        exit 1
+    fi
+}
+
+if [ -z "$INTERVAL" ]; then
+    INTERVAL=5
+    log 'WARNING' "INTERVAL is undefined, default is $INTERVAL seconds"
+fi
+
+if [ -z "$HOST" ]; then
+    HOST="google.com"
+    log 'WARNING' "HOST is undefined, default is $HOST"
+fi
+
+if [ -z "$PORT" ]; then
+    PORT=80
+    log 'WARNING' "PORT is undefined, default is $PORT"
+fi
+
+if [ -z "$TIMEOUT" ]; then
+    TIMEOUT=300
+    log 'WARNING' "TIMEOUT is undefined, default is $TIMEOUT seconds"
+fi
 
 until nc -z -v -w90 $HOST $PORT
 do
     TIMEOUT=`expr $TIMEOUT - $INTERVAL`
-    if [ $TIMEOUT -lt 0 ]
-    then
+    if [ $TIMEOUT -lt 0 ]; then
         break
     fi
 
